6ff2f3c268a420a90061d54e614c6bf1318eb921
Adarsha A <adarshaadi1997@gmail.com>
2022-10-20 19:35:30 +0530

    Showing Meals on Screen

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -30,14 +32,21 @@ const DUMMY_MEALS = [
 
 
 const AvailableMeals =() =>{
-    const mealsList = DUMMY_MEALS.map(meal =><li>{meal.name}</li>)
+    const mealsList = DUMMY_MEALS.map((meal) =>(
+    <MealItem 
+        key={meal.id} 
+        name={meal.name} 
+        description={meal.description} 
+        price = {meal.price}
+    />
+    ));
     return(
-        <section className={classes.meals}>
+        <Card className= {classes.meals} >
             <ul>
                 {mealsList}
                 
             </ul>
-        </section>
+        </Card>
     )
 }
 
