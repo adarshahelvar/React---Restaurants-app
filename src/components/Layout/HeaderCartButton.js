69bb866aa093092b0c092069ae70eaad6a6c0a2e
Adarsha A <adarshaadi1997@gmail.com>
2022-10-21 18:08:52 +0530

    Context API (cart item counter code)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useContext} from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../Store/cart-context";
 import classes from './HeaderCartButton.module.css';
+
 const HeaderCartButton = (props) =>{
+    
+const cartCtx = useContext(CartContext)
+const nuberOfCartItem = cartCtx.items.reduce((currNumber, item) =>{
+    return currNumber + item.amount;
+} , 0);             // reduce take two parameters one is fumction and second is Starting value
     return(
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -9,7 +16,7 @@ const HeaderCartButton = (props) =>{
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-            0
+            {nuberOfCartItem}
             </span>
         </button>
     )
